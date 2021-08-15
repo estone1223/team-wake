bf4a5d90094a8f0285281ccbd1455851deb4fe1d
estone <estone111223@gmail.com>
2021-08-15 20:04:55 +0900

    def model added null

--- group/models.py
+++ group/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Clan(models.Model):
     name = models.CharField('クラン名', max_length=12)
-    description = models.TextField('説明文', max_length=255)
+    description = models.TextField('説明文', max_length=255, null=False)
     
     class Meta:
         db_table = 'clans'
@@ -16,7 +16,7 @@ class Member(models.Model):
     name = models.CharField('名前', max_length=12, blank=False)
     is_Sniper = models.BooleanField('SRか', default=False)
     clan = models.ForeignKey(Clan, on_delete=models.SET_NULL)
-    memo = models.TextField('メモ', max_length=255)
+    memo = models.TextField('メモ', max_length=255, null=True)
 
     class Meta:
         db_table = 'members'
