46569e2bf591239880fc007bacbf29a11be95186
estone <estone111223@gmail.com>
2021-08-15 20:30:31 +0900

    added urls and views

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,13 @@
+from group.views import top
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
+
 
 urlpatterns = [
+    path('/top', top, name='top'),
+    path('/group', include('group.urls'), name='group'),
     path('admin/', admin.site.urls),
+    
 ]
