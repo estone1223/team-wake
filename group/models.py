3be89332e357342ea34d32f7203fb4afbc090652
estone <estone111223@gmail.com>
2021-08-15 20:02:54 +0900

    def model

--- group/models.py
+++ group/models.py
@@ -2,3 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+class Clan(models.Model):
+    name = models.CharField('クラン名', max_length=12)
+    description = models.TextField('説明文', max_length=255)
+    
+    class Meta:
+        db_table = 'clans'
+
+    def __str__(self):
+        return self.name
+
+class Member(models.Model):
+    name = models.CharField('名前', max_length=12, blank=False)
+    is_Sniper = models.BooleanField('SRか', default=False)
+    clan = models.ForeignKey(Clan, on_delete=models.SET_NULL)
+    memo = models.TextField('メモ', max_length=255)
+
+    class Meta:
+        db_table = 'members'
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
