49ad31f9315660a653777158e3ae78ee71c4fd24
estone <estone111223@gmail.com>
2021-08-17 17:44:49 +0900

    editted settings-loginurl

--- config/settings.py
+++ config/settings.py
@@ -117,3 +117,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login url
+
+LOGIN_URL = '/accounts/login/'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
