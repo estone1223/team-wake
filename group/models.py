f4f455a76ec1d1abdef8e43b6d9d7d75bbbe9c4f
estone <estone111223@gmail.com>
2021-08-19 20:04:06 +0900

    editted Model

--- group/models.py
+++ group/models.py
@@ -11,21 +11,9 @@ class Wake(models.Model):
     def __str__(self):
         return self.name
 
-# class Clan(models.Model):
-#     name = models.CharField('クラン名', max_length=12)
-#     description = models.TextField('説明文', max_length=255, null=False)
-    
-#     class Meta:
-#         db_table = 'clans'
-
-#     def __str__(self):
-#         return self.name
 
 class Member(models.Model):
     name = models.CharField('名前', max_length=12, blank=False)
-    # is_Sniper = models.BooleanField('SRか', default=False)
-    # # clan = models.ForeignKey(Clan, on_delete=models.SET_NULL, null=True, blank=True)
-    # memo = models.TextField('メモ', max_length=255, blank=True, null=True)
 
     class Meta:
         db_table = 'members'
