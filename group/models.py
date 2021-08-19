e6ce38fefc2d16204870991fea99bb25326f1ef2
estone <estone111223@gmail.com>
2021-08-19 20:00:03 +0900

    editted Model

--- group/models.py
+++ group/models.py
@@ -1,23 +1,31 @@
 from django.db import models
-from django.db.models.base import Model
 
 # Create your models here.
 
-class Clan(models.Model):
-    name = models.CharField('クラン名', max_length=12)
-    description = models.TextField('説明文', max_length=255, null=False)
+class Wake(models.Model):
+    name = models.CharField(('Wake名'), max_length=50)
     
     class Meta:
-        db_table = 'clans'
+        db_table = 'wakes'
 
     def __str__(self):
         return self.name
 
+# class Clan(models.Model):
+#     name = models.CharField('クラン名', max_length=12)
+#     description = models.TextField('説明文', max_length=255, null=False)
+    
+#     class Meta:
+#         db_table = 'clans'
+
+#     def __str__(self):
+#         return self.name
+
 class Member(models.Model):
     name = models.CharField('名前', max_length=12, blank=False)
-    is_Sniper = models.BooleanField('SRか', default=False)
-    clan = models.ForeignKey(Clan, on_delete=models.SET_NULL, null=True, blank=True)
-    memo = models.TextField('メモ', max_length=255, blank=True, null=True)
+    # is_Sniper = models.BooleanField('SRか', default=False)
+    # # clan = models.ForeignKey(Clan, on_delete=models.SET_NULL, null=True, blank=True)
+    # memo = models.TextField('メモ', max_length=255, blank=True, null=True)
 
     class Meta:
         db_table = 'members'
@@ -26,10 +34,12 @@ class Member(models.Model):
         return self.name
 
 
-# class Wake(models.Model):
-#     name = models.CharField(('Wake名'), max_length=50)
-#     team_a = models.List
+class MemberWakes(models.Model):
+    member_id = models.ForeignKey(Member, on_delete=models.CASCADE, blank=False, null=False)
+    wake_id = models.ForeignKey(Wake, on_delete=models.CASCADE, blank=False, null=False)
 
+    class Meta:
+        db_table = 'member_wakes'
 
 
 
