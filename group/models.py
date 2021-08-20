7cd318efa855760e180a20f4e91bd1d759a9f263
estone <estone111223@gmail.com>
2021-08-20 15:04:51 +0900

    before revert2

--- group/models.py
+++ group/models.py
@@ -4,31 +4,15 @@ from django.db import models
 
 class Member(models.Model):
     name = models.CharField('名前', max_length=12, blank=False)
-<<<<<<< HEAD
-=======
-    is_Sniper = models.BooleanField('SRか', default=False)
-    clan = models.ForeignKey(Clan, on_delete=models.SET_NULL, null=True)
-    memo = models.TextField('メモ', max_length=255, blank=True, null=True)
->>>>>>> parent of 7a865a3 (editted templates next->create model)
 
-    class Meta:
-        db_table = 'members'
 
     def __str__(self):
         return self.name
 
 
-<<<<<<< HEAD
 class Wake(models.Model):
     name = models.CharField(('Wake名'), max_length=50)
     member = models.ManyToManyField(Member, related_name='wakes', related_query_name='wake')
-    class Meta:
-        db_table = 'wakes'
 
     def __str__(self):
         return self.name
-=======
->>>>>>> parent of 7a865a3 (editted templates next->create model)
-
-
-
