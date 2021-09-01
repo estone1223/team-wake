545be6eb5ad878d4d6ef08c677acfec85d9200ab
yahomi-jp <estone111223@gmail.com>
2021-09-01 18:19:39 +0900

    completed js-p-type next->save member-log

--- static/wakes/js/wakes.js
+++ static/wakes/js/wakes.js
@@ -8,45 +8,37 @@ function wakeTeam() {
   team_b.textContent = '';
 
   // メンバーリストの取得
-  let test = []
+
   const selected_member = document.getElementsByName('selected-member');
+
+  // // 配分用のリストの作成
+  let user_list = [];
 
   for (let i = 0, len = selected_member.length; i < len; i++) {
     if (selected_member[i].checked) {
-      test.push(selected_member[i].value);
+      user_list.push(selected_member[i].value);
     }
   }
 
-  alert(test);
+  // 配分用リストの要素に重みをつけ、ソート
+  for (let i = 0, len = user_list.length; i < len; i++) {
+    user_list[i] = [user_list[i], Math.random()];
+    }
 
-  // // 配分用のリストの作成
-  // let user_list = [];
+  user_list.sort(function(a, b){return a[1] - b[1];});
 
-  // for (let i = 0, len = member.length; i < len; i++) {
-  //   if (member[i] != '') {
-  //     user_list.push(member[i]);
-  //   }
-  // }
+  // ソート済みの要素から重みを取り除く処理
+  for (let i = 0, len = user_list.length; i < len; i++) {
+    user_list[i] = user_list[i][0];
+  }
 
-  // // 配分用リストの要素に重みをつけ、ソート
-  // for (let i = 0, len = user_list.length; i < len; i++) {
-  //   user_list[i] = [user_list[i], Math.random()];
-  //   }
-
-  // user_list.sort(function(a, b){return a[1] - b[1];});
-
-  // // ソート済みの要素から重みを取り除く処理
-  // for (let i = 0, len = user_list.length; i < len; i++) {
-  //   user_list[i] = user_list[i][0];
-  // }
-
-  // //チーム分け処理 　
-  // for (let i = 0, len = user_list.length; i < len; i ++) {
-  //   if (i % 2 == 1) {
-  //     team_b.insertAdjacentHTML("beforeend", "<li class='team-member'>" + user_list[i] + "</li>");
-  //   } else {
-  //     team_a.insertAdjacentHTML("beforeend", "<li class='team-member'>" + user_list[i] + "</li>");
-  //   }
-  // }
+  //チーム分け処理 　
+  for (let i = 0, len = user_list.length; i < len; i ++) {
+    if (i % 2 == 1) {
+      team_b.insertAdjacentHTML("beforeend", "<li class='team-member'>" + user_list[i] + "</li>");
+    } else {
+      team_a.insertAdjacentHTML("beforeend", "<li class='team-member'>" + user_list[i] + "</li>");
+    }
+  }
   }
 
