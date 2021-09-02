24479579eb0c393a4ebfe51ab824ec850af3e662
yahomi-jp <estone111223@gmail.com>
2021-09-02 09:46:23 +0900

    config for Heroku-deploy

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,5 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
