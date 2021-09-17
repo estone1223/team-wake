d448d2c49dbd63b1aecc8a19a799004ccc7ecfe4
yahomi-jp <estone111223@gmail.com>
2021-09-17 19:27:40 +0900

    edited selectmemberform no mitame

--- wakes/form.py
+++ wakes/form.py
@@ -23,7 +23,10 @@ class SelectMemberForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         user_id = kwargs.get('instance').created_by.id
+        self.fields['member'].widget = forms.CheckboxSelectMultiple()
         self.fields['member'].queryset = Member.objects.filter(created_by=user_id)
+        
+
 
 
     class Meta:
